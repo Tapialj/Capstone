4c9c9e4924260a9dfff0fe8eb3dfd4e2330e5bb0
Tapialj <ljptapia@gmail.com>
2021-12-08 18:22:22 -0600

    Create actor views and components

---
+++ src/frontend/src/components/actor/Actor.vue
@@ -0,0 +1,58 @@
+<template>
+  <td class="id">{{ listNumber }}</td>
+  <td class="name">
+    <router-link :to="{ name: 'ActorDetails', params: { id: actor.id}}">
+      {{ actor.firstName }} {{ actor.lastName }}
+    </router-link>
+  </td>
+  <td class="delete">
+    <p @click="onDelete(actor)">Delete</p>
+  </td>
+</template>
+
+<script>
+  export default {
+    name: "Actor",
+    props: {
+      actor: Object,
+      listNumber: Number,
+    },
+    methods: {
+      onDelete(actor) {
+        this.$emit("onDelete", actor);
+      }
+    },
+    emits: [
+      "onDelete"
+    ],
+  }
+</script>
+
+<style scoped>
+  td {
+    border-bottom: 1px solid #00A38B;/*cecfd5;*/
+    text-align: left;
+    padding: 10px 15px;
+  }
+
+  .id {
+    width: 5%;
+  }
+
+  .name {
+    width: 55%;
+    color: #007582;
+    font-weight: bold;
+  }
+
+  .delete {
+    color: #3366BB;
+    text-align: right;
+  }
+
+  .delete p {
+    display: inline-block;
+    margin: 0;
+    cursor: pointer;
+  }
+</style>
